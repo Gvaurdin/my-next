db1c9667f2da18b9d8713466a05d84a2631bd399
Gvaurdin <gvaurdin777@yandex.ru>
2024-10-14 21:24:10 +0300

    привет

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home() {
       >
         <main className={styles.main}>
           Hello NextJS!!
+          Привет!
           <Image
             className={styles.logo}
             src="https://nextjs.org/icons/next.svg"
